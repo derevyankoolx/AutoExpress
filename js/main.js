56ece618f91a144352ea2ecc0df1c7a920365f67
devorx <derevyankoolx@gmail.com>
2022-09-09 19:51:45 +0300

    finish adaptive

--- js/main.js
+++ js/main.js
@@ -6,6 +6,21 @@ $(function () {
         slidesToShow: 3,
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 601,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+
+        ]
     });
 
 });
